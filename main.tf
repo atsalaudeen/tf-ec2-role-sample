8051ce727d54b6c5da70380ad427afe3e3803365
EC2 Default User <ec2-user@ip-172-31-95-185.ec2.internal>
2019-07-22 11:48:24 +0000

    provisioner updates

--- main.tf
+++ main.tf
@@ -7,18 +7,18 @@ provider "aws" {
 resource "aws_instance" "stadevtest2" {
   ami = "ami-0b898040803850657"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.testssh2.id}"]
+  vpc_security_group_ids = ["${aws_security_group.testappserver1-sg.id}"]
 
   # use this to attach existing role
   #iam_instance_profile = "tfadmin"
-  
+
   # use if creating new key below
   #key_name = "deployer-key"
-  
+
   # use for existing key
   key_name = "sta-acc-only"
-  
-  # attach new profile to be created 
+
+  # attach new profile to be created
   iam_instance_profile = "test_profile"
   tags = {
     Name = "Dev-Test4"
@@ -36,21 +36,37 @@ resource "aws_instance" "stadevtest2" {
     #! /bin/bash
     sudo yum update -y
     sudo amazon-linux-extras install -y php7.3
-    sudo yum install -y httpd nginx
-    php -v
+    sudo yum install -y httpd php gd php-gd php-xml php-json php-mbstring php-process php-common php-zip php-mysqlnd php-opcache
+    #php -v
+    sudo yum install -y nfs-utils
+    sudo yum install -y cachefilesd
+    sudo yum install -y java-1.8.0-openjdk.x86_64
     sudo yum install -y php-soap.x86_64
-    sudo systemctl start httpd
-    sudo systemctl enable httpd
-    sudo systemctl start nginx
-    sudo systemctl enable nginx 
+    sudo service cachefilesd start
+    sudo chkconfig cachefilesd on
     sudo mkdir -p /var/www/html/web/www
-    sudo wget https://releases.hashicorp.com/terraform/0.12.3/terraform_0.12.3_linux_amd64.zip
-    sudo unzip terraform_0.12.3_linux_amd64.zip
-    sudo cat > /var/www/html/web/www/index.html <<'_END'
-          <h1>Welcome to testing env</h1>
-          <p>
-          This is first ec2 app server. 
+
+    # This is just a demo. We use provisioner later 
+    sudo cat > /var/www/html/web/www/index.test <<'_END'
+          <html lang="en" dir="ltr">
+            <head>
+              <meta charset="utf-8">
+              <title>Welcome to Test Demo Page</title>
+              <style media="screen">
+                body {
+                  background-color: #3FBFBF;
+                }
+              </style>
+            </head>
+            <body> 
+            <h1>Welcome to test env 1</h1>
+            <p>Testing webservers</p>
+            <p>This is traffic from node 1</p>
+          
+            </body>
+          </html>
         -END
+
   EOF
 
   # Local provisioner to get ip address
@@ -58,28 +74,63 @@ resource "aws_instance" "stadevtest2" {
     command = "echo ${aws_instance.stadevtest2.private_ip} >> appserver_private_ips.txt"
   }
 
-  # Copies the myapp.conf file to /tmp/myapp.conf
+}
+
+# Null resourse that Copies the apache config, index.html to server and restarts apache
+  resource "null_resource" "provision_webserver1" {
+    connection {
+        type        = "ssh"
+        user        = "ec2-user"
+        #private_key = "${var.private_key}"
+        private_key = "${file("/home/ec2-user/.ssh/test.pem")}"
+        host = "${aws_instance.stadevtest2.public_ip}"
+    }
+
+    # Provisioner to copy apache conf file 
+    
+    provisioner "file" {
+         source      = "conf/apache-overrides.conf"
+         destination = "/tmp/apache-overrides.conf"
+    }
+    
+    # add remote exec provisioner to be able to copy file with correct permision 
+    provisioner "remote-exec" {
+      inline = [
+        "sudo chown root:root /tmp/apache-overrides.conf",
+        "sudo mv /tmp/apache-overrides.conf /etc/httpd/conf.d/apache-overrides.conf",
+      ]
+    }
+
+    # provisioner to copy index.html and restart apache 
+
   provisioner "file" {
-    source      = "conf/myapp.conf"
-    destination = "/tmp/myapp.conf"
+       source      = "conf/index.html"
+       destination = "/tmp/index.html"
+  }
+  
+  # add remote exec provisioner to be able to copy file with correct permision 
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chown root:root /tmp/index.html",
+      "sudo mv /tmp/index.html /var/www/html/web/www/index.html",
+      "sudo systemctl restart httpd",
+    ]
   }
 
-  provisioner "local-exec" {
-    command = "echo `php -v` >> appserver_private_ips.txt"
-  }
+  depends_on = ["aws_security_group.testappserver1-sg"]
 
 }
 
-# Create keypair to add ssh key to the instance 
+# Create keypair to add ssh key to the instance
 # skip if already created on aws
 #resource "aws_key_pair" "deployerkey" {
 #  key_name = "deployer-key"
-#  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCdPj/e8HJkSRzcJWCyHRAfq61Po0POfu33rxJiEYviGKrpWxN7G9uadfrogzrlCCesilXi3wNw641jczagDLFKmb72EYtUxnhmi8ba9ouOUnIhO6Vurifq/oep7+jkLvl8jjhgg90f2r44gabaKHKrU9jkuk0ib1mD test-only"
+#  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQASRzcJWCyHRAfq61Po0POfu33rxJiEYviGKrpWxNfrogzrlCCesilXi3wNw641jczagDLFKmb72EYtUxnhm878IhO6Vurifq/oep7+jkLvl8jjhgg90f2r44gabaKHKrU9jkuk0ib1mD test-only"
 #}
 
 # create and attach a profile to this instance
 
-# create profile 
+# create profile
 
 resource "aws_iam_instance_profile" "test_profile" {
   name = "test_profile"
@@ -87,7 +138,7 @@ resource "aws_iam_instance_profile" "test_profile" {
 }
 
 
-# create role 
+# create role
 resource "aws_iam_role" "sta_admin_role" {
   name = "TF_Admin_Role"
   description = "Statech admin role for tform"
@@ -113,7 +164,7 @@ EOF
 }
 
 
-# create the policy 
+# create the policy
 
 
 resource "aws_iam_policy" "tf_policy" {
@@ -136,7 +187,7 @@ resource "aws_iam_policy" "tf_policy" {
 EOF
 }
 
-# attach read only policy 
+# attach read only policy
 resource "aws_iam_role_policy_attachment" "ec2-read-only-policy-attachment" {
     role = "${aws_iam_role.sta_admin_role.name}"
     policy_arn = "${aws_iam_policy.tf_policy.arn}"
@@ -147,3 +198,4 @@ resource "aws_iam_role_policy_attachment" "ec2-read-only-policy-attachment" {
 output "nodeipaddress" {
   value = "${aws_instance.stadevtest2.public_ip}"
 }
+
--- sg_testssh.tf
+++ sg_testssh.tf
@@ -1,21 +1,27 @@
-resource "aws_security_group" "testssh2" {
-  name = "testssh2" 
+resource "aws_security_group" "testappserver1-sg" {
+  name = "testappserver-sg" 
   description = "security group for ssh and http access to testenv"
 
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["172.31.0.0/16"]
-  }
 
   ingress {
     from_port = 22
     to_port = 22
     protocol = "tcp"
     cidr_blocks = ["81.145.139.130/32"]
+    description = "Test Mgmt Network"
   }
 
+# narrow down to specific security group
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    #security_groups = ["sg-0fca1a86b3f4ebd6f"]
+    #security_groups = ["${aws_security_group.tf-mgmtserver-sg.id}"]
+  }
+
+#
   ingress {
     from_port = 80
     to_port = 80
@@ -31,6 +37,6 @@ resource "aws_security_group" "testssh2" {
   }
 
   tags = {
-    Name = "sg-dev1"
+    Name = "sg-testappserver1"
   }
 }
